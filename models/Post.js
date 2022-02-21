565bf0142a6a82507fa3032bc4445f8d48ca2e00
mukukuu <qinii@me.com>
2022-02-20 18:53:07 -0700

    added routes in post api controller

--- models/Post.js
+++ models/Post.js
@@ -16,7 +16,7 @@ class Post extends Model {
           'post_url',
           'title',
           'created_at',
-          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+          [sequelize.literal('(SELECT COUNT(*) FROM likes WHERE post.id = likes.post_id)'), 'likes_count']
         ],
         include: [
           {
